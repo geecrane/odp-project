ac93bbac1f7267f12815fb04e3f568fa73ead9c4
Hannodje <sfoucher@student.ethz.ch>
2017-05-15 23:01:22 +0200

    3rd try

---
+++ src/main/java/jUnitTests/DatabaseApiTest.java
@@ -0,0 +1,56 @@
+package jUnitTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import ch.ethz.globis.mtfobu.domains.Proceedings;
+import ch.ethz.globis.mtfobu.domains.Publisher;
+import ch.ethz.globis.mtfobu.odb_project.db.Database;
+import ch.ethz.globis.mtfobu.odb_project.db.DatabaseMongoDB;
+
+public class DatabaseApiTest {
+	private Database db;
+
+	public DatabaseApiTest() {
+		System.out.println("Testing: MongoDB");
+		DatabaseMongoDB.verbose = true;
+		db = new DatabaseMongoDB();
+
+		// test if db exists
+		assertNotEquals(null, db);
+
+		db.openDB();
+	}
+
+	@Test
+	public void testGetPublishersAndGetPublisherByName() {
+		// test getPublishers
+		List<Publisher> pubs = db.getPublishers();
+		assertEquals(93, pubs.size());
+		for (Publisher pub : pubs) {
+			// test getPublisherByName
+			Publisher foundPub = db.getPublisherByName(pub.getName());
+			assertNotEquals(null, foundPub);
+			assertEquals(pub.getId(), foundPub.getId());
+		}
+
+	}
+
+	@Test
+	public void testGetProceedings() {
+		// test getProceedings
+		List<Proceedings> procs = db.getProceedings();
+		assertEquals(1259, procs.size());
+		for (Proceedings proc : procs) {
+			assertNotEquals(null, proc.getId());
+			assertNotEquals(null, proc.getTitle());
+			assertNotEquals(null, proc.getPublisher());
+			assertNotEquals(null, proc.getPublications());
+			assertNotEquals(null, proc.getYear());
+		}
+	}
+}
